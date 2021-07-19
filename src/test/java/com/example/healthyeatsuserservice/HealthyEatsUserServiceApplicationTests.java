4638d460ea82945d4143a9cdf468a085af8c446e
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-07-19 14:46:43 +0100

    set up user-service

---
+++ src/test/java/com/example/healthyeatsuserservice/HealthyEatsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthyeatsuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyEatsUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
