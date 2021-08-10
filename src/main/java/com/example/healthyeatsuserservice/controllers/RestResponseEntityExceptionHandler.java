cd122d09848496f7cb83cfcf716d0a089fff695e
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-08-10 14:17:30 +0100

    updated controller advice error handling

--- src/main/java/com/example/healthyeatsuserservice/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/healthyeatsuserservice/controllers/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.healthyeatsuserservice.controllers;
 
+import com.example.healthyeatsuserservice.controllers.responses.APIResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -21,17 +25,20 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 
     @ExceptionHandler({BadCredentialsException.class})
     public ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException exception, WebRequest request) {
-        return new ResponseEntity<Object>("Incorrect username or password", new HttpHeaders(), HttpStatus.FORBIDDEN);
+        APIResponse response = new APIResponse(false,"Incorrect username or password" , exception.getMessage());
+        return new ResponseEntity<Object>(response, new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler({AccessDeniedException.class})
     public ResponseEntity<Object> handleAccessDeniedException(AccessDeniedException exception, WebRequest request) {
+        APIResponse response = new APIResponse(false,"You do not have permission to perform that action" , exception.getMessage());
         return new ResponseEntity<Object>(
-                "You do not have permission to perform that action", new HttpHeaders(), HttpStatus.FORBIDDEN);
+               response , new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Object> handleFallThroughException(Exception exception, WebRequest request) {
-        return new ResponseEntity<Object>(exception.getMessage(), new HttpHeaders(), HttpStatus.FORBIDDEN);
+        APIResponse response = new APIResponse(false, exception.getMessage());
+        return new ResponseEntity<Object>(response, new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
 }
--- src/main/java/com/example/healthyeatsuserservice/controllers/responses/APIResponse.java
+++ src/main/java/com/example/healthyeatsuserservice/controllers/responses/APIResponse.java
@@ -9,11 +9,18 @@ public class APIResponse {
     private boolean isSuccessful;
     private String message;
     private LocalDateTime timeStamp;
+    private String extraInfo;
 
     public APIResponse(boolean isSuccessful, String message) {
         this.isSuccessful = isSuccessful;
         this.message = message;
         timeStamp = LocalDateTime.now();
+    }
 
+    public APIResponse(boolean isSuccessful, String message, String extraInfo) {
+        this.isSuccessful = isSuccessful;
+        this.message = message;
+        timeStamp = LocalDateTime.now();
+        this.extraInfo = extraInfo;
     }
 }
