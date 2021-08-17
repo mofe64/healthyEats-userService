2d93788df6013a0c865080fd59d8fcf64df4410e
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-08-17 16:00:35 +0100

    cors filter test

--- src/main/java/com/example/healthyeatsuserservice/service/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/healthyeatsuserservice/service/security/ApplicationSecurityConfig.java
@@ -34,7 +34,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/**/auth/login", "/**/auth/register/**", "/**/auth/password/reset/**").permitAll()
                 .anyRequest().authenticated()
